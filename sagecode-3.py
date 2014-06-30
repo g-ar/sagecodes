f3215123eb37b609206d689b80c283968bb883ad
gar <not@home.moc>
2014-06-30 20:20:34 +0530

    added (17)

--- sagecode-3.py
+++ sagecode-3.py
@@ -70,3 +70,85 @@ n=[4]*10+[16]
 nn=8
 sum(sum(a*sum(sum([binomial(n[a],i)*binomial(n[a-1]*(a-1),nn-i-j)*binomial(52-n[a]-(a-1)*n[a-1],j) for i in [nn-k-j+1..n[a]]]) for j in [nn-k-n[a]+1..nn-k]) for a in [1..10]) for k in [6..8])/binomial(52,8)
 
+# (16) https://math.stackexchange.com/questions/846442/generating-function-probability-regaridng-coin-toss/
+
+var('h t')
+fht=1/(1-(t+h*(t+h*(t+h*(t+h*(t+h*t))))))*(1+h*(1+h*(1+h*(1+h*(1+h)))))
+fhtt=taylor(fht,(h,0),(t,0),25)
+fhtt.coeff(t,8).coeff(h,17)/2^25
+
+# (17) http://math.stackexchange.com/questions/850857/an-archery-game/851331#851331
+
+def prob_consecutive(probA=1/2, probB=1/2, numA=2, numB=3, winner=0):
+    '''
+    probA and probB are player 0's and player 1's hitting probabilities
+    numA and numB are the number of times they need to consecutively hit the target to win
+    If winner=0, it calculates the probability for player 0 to win, similarly for winner=1.
+    '''
+    pa = probA
+    pb = probB
+    qa = 1-pa
+    qb = 1-pb
+    eqns = []
+    varstr = ''
+    nA = numA
+    nB = numB
+    nn = max(nA,nB)
+    ql = [[0]*(nn+2) for i in [0..nn+2]]
+    d1 = {}
+    q  =  var(','.join('q%s%s'%(i,j) for i in range(nA+1) for j in range(nB+1)));
+    for i in range(0,nA+1):
+        for j in range(0,nB+1):
+            if i<nA and j>=nB:
+                ql[i][j] = winner
+            if i>=nA and j<nB:          
+                ql[i][j] = 1-winner
+            if i<nA and j<nB:
+                ql[i][j] = q[(nB+1)*i+j]
+    for i in range(0,nA+1):
+        for j in range(0,nB+1):
+            if i<nA and j<nB:
+                eqns.append(q[(nB+1)*i+j] == pa*qb*ql[i+1][0]+qa*pb*ql[0][j+1]+pa*pb*ql[i+1][j+1]+qa*qb*ql[0][0]) 
+                d1[str(q[(nB+1)*i+j])] = q[(nB+1)*i+j]           
+    varstr = ','.join('q%s%s'%(i,j) for i in range((nB+1)) for j in range((nB+1)) if i<nA and j<nB) 
+    sols = sage_eval('solve('+str(eqns)+','+varstr+')',locals = d1);
+    return (sols[0][0]).rhs()
+prob_consecutive(3/4,43/50,10,12)
+
+# Slightly modified equation for the winner when the constraint of consecutive hits is removed.
+
+def prob_nonconsecutive(probA=1/2, probB=1/2, numA=2, numB=3, winner=0):
+    '''
+    probA and probB are player 0's and player 1's hitting probabilities
+    numA and numB are the number of times they need to hit the target to win
+    If winner=0, it calculates the probability for player 0 to win, similarly for winner=1.
+    '''
+    pa = probA
+    pb = probB
+    qa = 1-pa
+    qb = 1-pb
+    eqns = []
+    varstr = ''
+    nA = numA
+    nB = numB
+    nn = max(nA,nB)
+    ql = [[0]*(nn+2) for i in [0..nn+2]]
+    d1 = {}
+    q  =  var(','.join('q%s%s'%(i,j) for i in range(nA+1) for j in range(nB+1)));
+    for i in range(0,nA+1):
+        for j in range(0,nB+1):
+            if i<nA and j>=nB:
+                ql[i][j] = winner
+            if i>=nA and j<nB:          
+                ql[i][j] = 1-winner
+            if i<nA and j<nB:
+                ql[i][j] = q[(nB+1)*i+j]
+    for i in range(0,nA+1):
+        for j in range(0,nB+1):
+            if i<nA and j<nB:
+                eqns.append(q[(nB+1)*i+j] == pa*qb*ql[i+1][j]+qa*pb*ql[i][j+1]+pa*pb*ql[i+1][j+1]+qa*qb*ql[i][j]) 
+                d1[str(q[(nB+1)*i+j])] = q[(nB+1)*i+j]           
+    varstr = ','.join('q%s%s'%(i,j) for i in range((nB+1)) for j in range((nB+1)) if i<nA and j<nB) 
+    sols = sage_eval('solve('+str(eqns)+','+varstr+')',locals = d1);
+    return (sols[0][0]).rhs()
+prob_nonconsecutive(3/4,43/50,10,12,1)    
