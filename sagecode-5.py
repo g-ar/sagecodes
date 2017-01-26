8d130ad2cdcc867f8c3e62c7936bab3a45327241
gar <g-ar@users.noreply.github.com>
2017-01-26 21:12:50 +0530

    added (22)

--- sagecode-5.py
+++ sagecode-5.py
@@ -16,3 +16,59 @@ for i in xrange(6):
 
 show(am)
 print sum((identity_matrix(5)-am[0:5,0:5]).inverse()[0].list())
+
+# (22) https://math.stackexchange.com/questions/2106229/flipping-coins-and-advantages/
+# Using a monte-carlo simulation
+import random
+
+ntrials = 1000000
+cnt = 0
+nh = 10  # total heads to win
+
+for _ in xrange(ntrials):
+    nha, nhb = 0, 0 # player a and b's number of heads obtained
+    while True:
+        if random.randint(0,1):
+            nha += 1
+        if nha == nh:
+            break
+        if random.randint(0,1):
+            nhb += 1
+        if nhb == nh:
+            break
+    if nha > nhb:
+        cnt += 1
+
+print cnt/float(ntrials)
+
+
+# Using an absorbing Markov chain
+def calc(n):
+    Bm = Matrix(QQ, n^2)
+    Am = Matrix(QQ, n^2)
+    Cm = Matrix(QQ, n^2, 2)
+    Dm = Matrix(QQ, n^2, 2)
+    Zm = Matrix(QQ, n^2)
+
+    for i in range(n):
+        for j in range(n):
+            Bm[n*i+j, n*i+j] = 1/2
+            if j+1 != n:
+                Bm[n*i+j, n*i+j+1] = 1/2
+            else:
+                Cm[n*i+j, 0] = 1/2
+
+    for i in range(n):
+        for j in range(n):
+            Am[n*i+j,n*i+j] = 1/2
+            if i+1 != n:
+                Am[n*i+j, n*(i+1)+j] = 1/2
+            else:
+                Dm[n*i+j, 1] = 1/2
+
+    Qm = Zm.augment(Am).stack(Bm.augment(Zm))
+    Bm = Dm.stack(Cm)
+    res = ((identity_matrix(n^2*2)-Qm).inverse()*Bm)[0]
+    return res
+
+print calc(10)[1]
