83cf283e153a0fea1fd94e6129a19148c85d89f8
gar <g-ar@users.noreply.github.com>
2020-05-01 22:20:06 +0530

    modified for custom input from a text file

--- numbermind.py
+++ numbermind.py
@@ -1,13 +1,30 @@
 from z3 import *
+import sys
 
 def main():
     sol = Solver()
-    cols = 5
+    fname = "input.txt"
+    is_sol_distinct = True
+    
+    if len(sys.argv) > 1:
+        fname = sys.argv[1]
+        
+    if len(sys.argv) > 2:
+        is_sol_distinct = sys.argv[2] == "1"
 
-    a = [[4,7,2,9,1],
-         [9,4,6,8,7],
-         [3,1,8,7,2],
-         [1,5,7,3,9]]
+    numbers = []
+    hints = []
+    
+    with open(fname, "r") as fo:
+        lines = fo.readlines()
+
+        for l in lines:
+            n, h = l.split(";")
+            numbers.append([int(i) for i in n.split(',')])
+            hints.append([int(i) for i in h.split(',')])
+            
+    cols = len(numbers[0])
+    a = numbers
     
     # variables
     x = [Int("x[%d]" % j) for j in range(cols)]
@@ -16,28 +33,28 @@ def main():
     '''
     All are distinct
     '''
-#    sol.add(Distinct(x))
+    if is_sol_distinct:
+        sol.add(Distinct(x))
   
     '''
     All numbers 0 <= x_i <= 9
     '''
     for i in range(cols): 
         sol.add(x[i] >= 0, x[i] <= 9)
-        
-    sol.add(Sum([If(x[c] != a[0][c],1,0) for c in range(cols)]) == 5) # no number in correct position
-    sol.add(Sum([If(x[c] != a[1][c],1,0) for c in range(cols)]) == 5) # no number in correct position
-    sol.add(Sum([If(x[c] != a[2][c],1,0) for c in range(cols)]) == 4) # one number in correct position
-    sol.add(Sum([If(x[c] != a[3][c],1,0) for c in range(cols)]) == 3) # two numbers in correct position
+
+    for i in range(len(a)):
+        sol.add(Sum([If(x[c] == a[i][c],1,0) for c in range(cols)]) == hints[i][1])
     
     cnt = 0
-  
     while sol.check() == sat:
         mod = sol.model()
         xval = [mod.eval(x[j]).as_long() for j in range(cols)]
-        if len(set(xval) & set(a[0])) == 1 and\
-           len(set(xval) & set(a[1])) == 1 and\
-           len(set(xval) & set(a[2])) == 2 and\
-           len(set(xval) & set(a[3])) == 2:    
+        cond = True
+
+        for i in range(len(a)):
+            cond &= (len(set(xval) & set(a[i])) == hints[i][0])
+            
+        if cond:
             cnt += 1
             print(xval)
         sol.add(Or([x[i] != mod.eval(x[i]).as_long() for i in range(cols)])) # add constraint to check for different solution
